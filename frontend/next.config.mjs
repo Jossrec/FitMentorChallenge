e6f859c75f9418babb1f5a415c0fb54a2e340e3b
Jossrec <a00837812@tec.mx>
2025-10-01 14:18:43 -0600

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  output: 'export',            // <- esto habilita el export estático
+  images: { unoptimized: true } // si usas next/image
+  // basePath y assetPrefix NO son necesarios en Cloudflare Pages
+};
 export default nextConfig;
